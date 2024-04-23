258903360cb6f38ce85fd345be2e779c89be8ba6
Miguel Gonzalez De Anda <mg5615126@gmail.com>
2024-04-22 21:07:46 -0600

    Reacomodo y creacion de carpea Programacion Java

---
+++ Programacion Java/MiPropuesta de Proyecto - copia/VentanaAves.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+//import java.awt.*;
+
+public class VentanaAves extends JFrame {
+
+    public VentanaAves() {
+        super("Aves"); // Título de la ventana
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Configurar el contenido de la ventana de aves
+
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    // Otros métodos y componentes específicos de la ventana de aves
+}
