253ecfbdb5229500947eb1ca26608d77db7f3703
Miguel Gonzalez De Anda <mg5615126@mail.com>
2025-04-12 15:47:49 -0600

    Reacomodo

---
+++ Fusion/Programacion Java/MiPropuesta de Proyecto - copia/VentanaPeces.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+//import java.awt.*;
+
+public class VentanaPeces extends JFrame {
+
+    public VentanaPeces() {
+        super("Peces"); // Título de la ventana
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Configurar el contenido de la ventana de peces
+
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    // Otros métodos y componentes específicos de la ventana de peces
+}
